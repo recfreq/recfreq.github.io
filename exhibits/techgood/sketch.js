a0c75fce37a00d773ab24ce6cd6412e20ddf2a4f
Ray LC <30538231+recfreq@users.noreply.github.com>
2019-07-03 17:40:23 -0400

    update

--- exhibits/techgood/sketch.js
+++ exhibits/techgood/sketch.js
@@ -1,28 +1,46 @@
 var canvas;
-var xpos = 0, ypos = 0, rad = 0;
+var circx = [], circy = [], circr = [];
+var posx = 0, posy = 0;
 
 function setup() {
 	canvas = createCanvas(windowWidth, windowHeight);
 	canvas.position(0, 0);
 	canvas.style('display', 'block');
 	canvas.style('z-index', '1');
-	noFill();
-	stroke(254,255,237);
-	strokeWeight(2);
 }
 
 function draw() {
-	if(xpos != 0 && ypos != 0) {
-		ellipse(xpos, ypos, rad, rad);
+	noFill();
+	stroke(254,255,237);
+	strokeWeight(2);
+	if(circx.length != 0 && circy.length != 0) {
+		for(var k = 0; k < circx.length; k++) {
+			ellipse(circx[k], circy[k], 2*circr[k], 2*circr[k]);
+		}
+	}
+	if(posx < circx[0]+100) {
+		posx++;
+		line(circx[0]+circr[0], circy[0], posx, posy);
+	} else if(circx.length < 2) {
+		circr[1] = random(3, 10);
+		circx[1] = posx+circr[1]; circy[1] = circy[0];
+		posx = circx[1]; posy = circy[1]+circr[1];
+	} else if(posy < circy[0]+100) {
+		posy++;
+		line(circx[1], circy[1]+circr[1], posx, posy);
+	} else if(circx.length < 3) {
+		circr[2] = random(3, 10);
+		circx[2] = circx[1]; circy[2] = posy+circr[2];
 	}
 }
 
 function mousePressed() {
 	clear();
-	xpos = mouseX;
-	ypos = mouseY;
-	rad = random(5, 20);
-	print(xpos + ', ' + ypos + ': ' + rad);
+	circx = []; circy = []; circr = [];
+	circx[0] = mouseX;	circy[0] = mouseY;
+	circr[0] = random(3, 10);
+	posx = circx[0]+circr[0]; posy = circy[0];
+	print(circx[0] + ', ' + circy[0] + ': ' + circr[0]);
 }
 
 function windowResized() {
---
+++ machinegaze/machinegaze.js
@@ -0,0 +1,182 @@
+var images = ["parsonsmachinegaze-01.jpg",
+"parsonsmachinegaze-17.jpg",
+"parsonsmachinegaze-02.jpg",
+"parsonsmachinegaze-18.jpg",
+"parsonsmachinegaze-03.jpg",
+"parsonsmachinegaze-19.jpg",
+"parsonsmachinegaze-04.jpg",
+"parsonsmachinegaze-20.jpg",
+"parsonsmachinegaze-05.jpg",
+"parsonsmachinegaze-21.jpg",
+"parsonsmachinegaze-06.jpg",
+"parsonsmachinegaze-22.jpg",
+"parsonsmachinegaze-07.jpg",
+"parsonsmachinegaze-23.jpg",
+"parsonsmachinegaze-08.jpg",
+"parsonsmachinegaze-24.jpg",
+"parsonsmachinegaze-09.jpg",
+"parsonsmachinegaze-25.jpg",
+"parsonsmachinegaze-10.jpg",
+"parsonsmachinegaze-26.jpg",
+"parsonsmachinegaze-11.jpg",
+"parsonsmachinegaze-27.jpg",
+"parsonsmachinegaze-12.jpg",
+"parsonsmachinegaze-28.jpg",
+"parsonsmachinegaze-13.jpg",
+"parsonsmachinegaze-29.jpg",
+"parsonsmachinegaze-14.jpg",
+"parsonsmachinegaze-30.jpg",
+"parsonsmachinegaze-15.jpg",
+"parsonsmachinegaze-31.jpg",
+"parsonsmachinegaze-16.jpg",
+"parsonsmachinegaze-32.jpg"];
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    loadImages();
+});
+
+//A variable storing the index in the image array of the image that's being showed in the popup
+var imgSwitch = 0;
+
+//Put the images in the columns in the order:1, 2, 3 ... 1, 2 ...
+function loadImages() {
+    //Getting the columns in html
+    var columns = document.getElementsByClassName("column");
+    var columnNum = columns.length;
+
+    //putInNum means which column the next image will be put into
+    var putInNum = 0;
+
+    //Check the number of images and the number of columns
+    console.log("Number of images: "+images.length);
+    console.log("Number of columns: "+columns.length);
+
+    //Use a for loop to put all the images into columns
+    for(var i = 0; i<images.length; i++){
+        //Loading the images into a new <img> tag
+        var newImg = document.createElement("img");
+        newImg.className = "images";
+
+        //Set the src attribute of the new <img> tag
+        newImg.src = "./images/"+images[i];
+
+        //ID is for the click event to track which image it is
+        newImg.id = i;
+
+        //can't use i here. must use the id of the image
+        //because if using i, it will always be the last index of the array when the window is loaded
+        newImg.addEventListener("click", function (e){
+            popUp(e.target.id);
+            imgSwitch = e.target.id;
+        });
+
+
+        //Put the new <img> tag into the column[putInNum]
+        columns[putInNum].appendChild(newImg);
+
+        //Move to the next column
+        putInNum++;
+
+        //If PutInNum is bigger than the number of columns, put the next image in the first column
+        //Why -1?
+        //Because the index of array starts from 0
+        if(putInNum>columnNum-1){
+            putInNum = 0;
+        }
+    }
+}
+
+//function to call popup
+function popUp(imgCount){
+    //record the index of the image showing in the popup
+    imgSwitch = imgCount;
+
+    var popup = document.getElementById("popup");
+    var img = document.getElementById("pic");
+
+    //make the popup div appear
+    popup.style.zIndex = "1";
+    popup.style.display = "block"
+
+    //if click on the image in the popup, the popup will be closed
+    img.addEventListener("click", function (e){
+        close();
+    });
+
+    //put the address of the image in <img>
+    img.src = "./images/"+images[imgCount];
+}
+
+//close the popup div
+function close(){
+    var popup = document.getElementById("popup");
+    popup.style.zIndex = "-1";
+    popup.style.display = "none"
+}
+
+//function for switching images
+function change(direction){
+    var numOfImg = images.length;
+
+    //A variable storing the index of next image
+    var next = 0;
+    var img = document.getElementById("pic");
+
+    //if direction is 1, switch to the right. If it's -1, switch to the left
+    if(direction==1){
+        //should be imgSwitch+1>=numOfImg
+        //don't use imgSwitch+1, it can be wrong. e.g. imgSwitch is 5, imgSwitch+1 is 51, not 6
+        //Careful when using "+"
+
+        //if the next index is larger than the array length, make the next index 0, else, next = imgSwitch+1
+        if(imgSwitch>numOfImg-2){
+            next = 0;
+        }else{
+            imgSwitch++;
+            next = imgSwitch;
+        }
+    }else if(direction==-1){
+        if(imgSwitch-1<0){
+            next = numOfImg-1;
+        }else{
+            next = imgSwitch-1;
+        }
+    }
+
+    //store the index of the showing image into imgSwitch
+    img.src = "./images/"+images[next];
+    imgSwitch = next;
+}
+
+/*"parsonsmachinegaze-01.jpg",
+"parsonsmachinegaze-02.jpg",
+"parsonsmachinegaze-03.jpg",
+"parsonsmachinegaze-04.jpg",
+"parsonsmachinegaze-05.jpg",
+"parsonsmachinegaze-06.jpg",
+"parsonsmachinegaze-07.jpg",
+"parsonsmachinegaze-08.jpg",
+"parsonsmachinegaze-09.jpg",
+"parsonsmachinegaze-10.jpg",
+"parsonsmachinegaze-11.jpg",
+"parsonsmachinegaze-12.jpg",
+"parsonsmachinegaze-13.jpg",
+"parsonsmachinegaze-14.jpg",
+"parsonsmachinegaze-15.jpg",
+"parsonsmachinegaze-16.jpg",
+"parsonsmachinegaze-17.jpg",
+"parsonsmachinegaze-18.jpg",
+"parsonsmachinegaze-19.jpg",
+"parsonsmachinegaze-20.jpg",
+"parsonsmachinegaze-21.jpg",
+"parsonsmachinegaze-22.jpg",
+"parsonsmachinegaze-23.jpg",
+"parsonsmachinegaze-24.jpg",
+"parsonsmachinegaze-25.jpg",
+"parsonsmachinegaze-26.jpg",
+"parsonsmachinegaze-27.jpg",
+"parsonsmachinegaze-28.jpg",
+"parsonsmachinegaze-29.jpg",
+"parsonsmachinegaze-30.jpg",
+"parsonsmachinegaze-31.jpg",
+"parsonsmachinegaze-32.jpg"*/
