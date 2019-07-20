b531de44bfbfb4730e6b339a58f724fd4a2db7d4
Ray LC <30538231+recfreq@users.noreply.github.com>
2019-07-20 01:19:00 -0400

    update

--- exhibits/techgood/script.js
+++ exhibits/techgood/script.js
@@ -5,6 +5,19 @@ $(function() {
   $(".resize").resizable();
 });
 
+var title = ["SHAMIMA: MEMORY IN MY HEART, BY RAY LC, FABEHA MONIR, ANIKA ULLAH",
+"AGAIN AGAIN, BY ANDREW YOON",
+"METABOLISM II, BY BOLOR AMGALAN",
+"LIFE IN UNDERGROUND, BY FABEHA MONIR",
+"HEALING DESTINATIONS, BY JONAH BRUCKNER-COHEN",
+"THE UNSPOKEN MONUMENT, BY JUNE LEE",
+"SELF PORTRAIT, BY KAT ZHANG",
+"UNTITLED, BY KE DING, SARA BRYN BIRCHARD",
+"PORTAL TO MIAMI, BY PILI LOPEZ",
+"WHITE TERROR, BY RICKY CAI",
+"IMMERSIVE REFUGEE VR EXPERIENCE, BY RAY LC, FABEHA MONIR, ANIKA ULLAH",
+"THE FUTURE OF REPRODUCTIVE LABOR, BY JUNE LEE"];
+
 //Vegas plugin for JQuery: http://vegas.jaysalvat.com/documentation/settings/
 //http://vegas.jaysalvat.com/documentation/transitions/
 $("body").vegas({
@@ -13,14 +26,24 @@ $("body").vegas({
 	transition: 'blur',
     transitionDuration: 1000,
     slides: [
-        { src: "images/techgood01.jpg" },
-        { src: "images/techgood02.jpg" },
-        { src: "images/techgood03.jpg" },
-        { src: "images/techgood04.jpg" },
-        { src: "images/techgood05.jpg" },
-        { src: "images/techgood06.jpg" },
-        { src: "images/techgood07.jpg" },
-        { src: "images/techgood08.jpg" }
+        { src: "images/RayLCFabehaMonirAnikaUllah01Shamima.jpg" },
+        { src: "images/AndrewYoon01AgainAgain.jpg" },
+        { src: "images/BolorAmgalan01MetabolismII.jpg" },
+        { src: "images/FabehaMonir01LifeInUnderground.jpg" },
+        { src: "images/JonahBrucknerCohen01HealingDestinations.jpg" },
+        { src: "images/JuneLee01UnspokenMonument.jpg" },
+        { src: "images/KatZhang01SelfPortrait.jpg" },
+        { src: "images/KeDingSaraBrynBirchard01Untitled.jpg" },
+        { src: "images/PiliLopez01PortalAMiMiami.jpg" },
+        { src: "images/RickyCai01WhiteTerror.jpg" },
+        { src: "images/RayLCFabehaMonirAnikaUllah02Rohingya.jpg" },
+        { src: "images/JuneLee02FutureOfReproductiveLabor.jpg" }
     ],
-    overlay: 'lib/vegas/overlays/02.png'
+    overlay: 'lib/vegas/overlays/02.png',
+    animation: 'random',
+    animationDuration: 6000,
+    walk: function (index, slideSettings) {
+        console.log("Slide index " + index + " image " + slideSettings.src);
+        document.getElementById("label").innerHTML = title[index];
+    }
 });
