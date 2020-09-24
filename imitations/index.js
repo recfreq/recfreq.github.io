753e0dbd00e9adffb0e6bf409d67e1a155b99239
Ray LC <30538231+recfreq@users.noreply.github.com>
2020-09-24 15:31:43 -0700

    imitations

---
+++ imitations/index.js
@@ -0,0 +1,13 @@
+//https://github.com/robin-dela/hover-effect
+new hoverEffect({
+    parent: document.querySelector('#graphic'),
+    intensity: 0.8,  //default 1
+    image1: './media/figs_contents02draw.png',
+    image2: './media/figs_contents03install.png',
+    displacementImage: './media/figs_contents04heightmap.png',
+    speedIn: 1.5,  //default 1.6
+    speedOut: 2.5,  //default 1.2
+    easing: 'Expo.power4',  //default Expo.easeOut
+    angle: Math.PI/4,  //default pi/4
+    imagesRatio: 526 / 960  //default to square 1
+});
