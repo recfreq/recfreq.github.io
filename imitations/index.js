008eda875292eff2a06b77d69a440a46281d7743
Ray LC <30538231+recfreq@users.noreply.github.com>
2020-10-10 12:29:45 -0700

    imitation draw

--- imitations/index.js
+++ imitations/index.js
@@ -3,7 +3,7 @@ new hoverEffect({
     parent: document.querySelector('#graphic'),
     intensity: 0.8,  //default 1
     image1: './media/figs_contents02draw.png',
-    image2: './media/figs_contents03install.png',
+    image2: './media/figs_contents04network.png',
     displacementImage: './media/figs_contents04heightmap.png',
     speedIn: 1.5,  //default 1.6
     speedOut: 2.5,  //default 1.2
